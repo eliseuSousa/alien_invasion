7382fa2635f1cc910cf647f8803f84068ea7ad72
eliseuSousa <eliseusousa759@gmail.com>
2021-10-16 08:51:21 -0300

    Refatorando check_events()

--- game_fuctions.py
+++ game_fuctions.py
@@ -2,24 +2,30 @@ import sys
 
 import pygame
 
+def check_events_keydown(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_events_keyup(event, ship):
+    if event.key == pygame.K_k:
+        sys.exit()
+    elif event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Responde a eventos do teclado e mouse."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True    
+            check_events_keydown(event, ship)    
         
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_k:
-                sys.exit()
-            elif event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_events_keyup(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """Atualiza as imagens na tela e altera para a nova tela"""
